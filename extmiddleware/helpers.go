58c7934be5ccb54cde072ac6534a58b424178ef0
Brody Over <brodyover@gmail.com>
2023-04-25 21:45:40 -0400

    add TrimmedQParam

--- extmiddleware/helpers.go
+++ extmiddleware/helpers.go
@@ -1,6 +1,9 @@
 package extmiddleware
 
-import "strings"
+import (
+	"net/http"
+	"strings"
+)
 
 func sanitizeURI(uri string) string {
 	// double slash `\\`, `//` or even `\/` is absolute uri for browsers and by redirecting request to that uri
@@ -9,4 +12,15 @@ func sanitizeURI(uri string) string {
 		uri = "/" + strings.TrimLeft(uri, `/\`)
 	}
 	return uri
+}
+
+// return the given query parameter with all leading and trailing white space removed, as defined by Unicode.
+func TrimmedQParam(r *http.Request, q string) string {
+	var qp = r.URL.Query().Get(q)
+
+	if qp != "" {
+		return strings.TrimSpace(qp)
+	}
+
+	return qp
 }
