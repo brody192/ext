0e592456dec628312b451b477379d6d9ed3980b1
Brody Over <brodyover@gmail.com>
2023-05-01 22:34:29 -0400

    add AutoReplyMap

--- extmiddleware/middlewares.go
+++ extmiddleware/middlewares.go
@@ -143,3 +143,29 @@ func AutoReply(paths []string, code int) func(http.Handler) http.Handler {
 		})
 	}
 }
+
+// auto reply to paths with the paths given status code
+//
+// given paths are pre-compiled to be clean, incoming paths are cleaned and checked against the cleaned paths
+func AutoReplyMap(pathCodes map[string]int) func(http.Handler) http.Handler {
+	var pathCodesClean = make(map[string]int, len(pathCodes))
+
+	for p, c := range pathCodes {
+		var cleanPath = path.Clean(p)
+
+		pathCodesClean[cleanPath] = c
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var urlPathClean = path.Clean(r.URL.Path)
+
+			if code, ok := pathCodesClean[urlPathClean]; ok {
+				w.WriteHeader(code)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
